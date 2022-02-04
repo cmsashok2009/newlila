10ad2c0b097467a48d067c7470bc18dfd487a9c3
ashok.kumar086 <ashokkumar.natarajan@samrantech.com>
2022-02-04 10:38:49 +0530

    Updates

---
+++ precache-manifest.cbe6b8c29641e4e5c93feb403aa98d19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d734b98b2301112fe67126e4bb84c5",
+    "url": "/newlila/index.html"
+  },
+  {
+    "revision": "192700b1bb1f79c7b369",
+    "url": "/newlila/static/css/main.6f4c0822.chunk.css"
+  },
+  {
+    "revision": "b2832388963e520a99c8",
+    "url": "/newlila/static/js/2.4aeff2c2.chunk.js"
+  },
+  {
+    "revision": "192700b1bb1f79c7b369",
+    "url": "/newlila/static/js/main.bfa288cd.chunk.js"
+  },
+  {
+    "revision": "4f0c934c4b3bf7b8df2b",
+    "url": "/newlila/static/js/runtime~main.39bebdf3.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/newlila/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/newlila/static/media/shopping-bag.30f07071.svg"
+  }
+]);
